e18cb676d45d6b868a382b80e0302eb12568a8a9
yeosang0508 <yeosang0508@gmail.com>
2024-10-23 02:20:14 +0900

    SSA : ResNet50 변경 후 전처리

--- VSCode/dog_behavior_analysis/scripts/test_model.py
+++ VSCode/dog_behavior_analysis/scripts/test_model.py
@@ -10,17 +10,30 @@ model_path = r'C:\Users\admin\IdeaProjects\TailsRoute_PJ\VScode\models\dog_behav
 model = load_model(model_path)
 print("모델 정상 작동")
 
-#테스트 이미지 로드 및 전처리
-image_path = r'C:\Users\admin\IdeaProjects\TailsRoute_PJ\VSCode\dog_behavior_analysis\data\sit\n02085620_4290.jpg' # 이미지 경로
-image = load_img(image_path, target_size=(224, 224)) # 이미지 크기 
-image = img_to_array(image) / 255.0 # 정류화 (0-1 사이 값으로 변환)
-image = np.expand_dims(image, axis=0) # 배치 차원 추가
+# OpenCV로 전처리
+def preprocess_image(model_path):
+    image = cv2.imread(model_path)
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    blurred_image = cv2.GaussianBlur(gray_image, (5, 5), 0)
+    edges = cv2.Canny(blurred_image, threshold1=50, threshold2=150)
+    processed_image = cv2.resize(edges, (224, 224))
+    processed_image = np.expand_dims(processed_image, axis=-1)
+    processed_image = np.expand_dims(processed_image, axis=0) / 255.0
+    return processed_image
+
+#테스트 이미지 로드
+image_path = r'C:\Users\admin\IdeaProjects\TailsRoute_PJ\VSCode\dog_behavior_analysis\data\other\n02085620_3033.jpg' # 이미지 경로
+processed_image = processed_image(image_path)
+
 
 # 예측 수행
-prediction = model.predict(image)
+prediction = model.predict(processed_image)
+
+# 결과 출력 
+prediction_percentage = prediction[0][0] * 100
 
 # 결과 출력
-if prediction[0][0] >= 0.5:
-    print(f"예측 결과: 앉기 행동 (확률: {prediction[0][0]:.2f})")
+if prediction_percentage >= 50:
+    print(f"예측 결과: 앉기 행동 (확률: {prediction_percentage:.2f}%)")
 else:
-    print(f"예측 결과: 기타 행동 (확률: {prediction[0][0]:.2f})")
+    print(f"예측 결과: 기타 행동 (확률: {prediction_percentage:.2f}%)")
