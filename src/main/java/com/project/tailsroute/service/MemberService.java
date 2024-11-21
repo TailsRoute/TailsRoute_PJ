cda47800a652d69f5ca4715d1dd0a64d98b97116
Eichi2514 <picon1317@gmail.com>
2024-11-21 11:32:49 +0900

    아이디 / 비밀번호 찾기, 메일 인증, 비밀번호 수정 기능 추가

--- src/main/java/com/project/tailsroute/service/MemberService.java
+++ src/main/java/com/project/tailsroute/service/MemberService.java
@@ -5,13 +5,23 @@ import com.project.tailsroute.util.Ut;
 import com.project.tailsroute.vo.Member;
 import com.project.tailsroute.vo.ResultData;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+
+import java.util.Random;
+import java.util.UUID;
 
 @Service
 public class MemberService {
 
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    private final Random random = new Random();
 
     public Member getMemberByLoginId(String loginId) {
         return memberRepository.getMemberByLoginId(loginId);
@@ -39,8 +49,8 @@ public class MemberService {
     }
 
 
-    public void memberModify(int loginedMemberId, String name, String nickname, String cellphoneNum) {
-        memberRepository.memberModify(loginedMemberId, name, nickname, cellphoneNum);
+    public void memberModify(int loginedMemberId, String name, String nickname, String cellphoneNum, String loginPw) {
+        memberRepository.memberModify(loginedMemberId, name, nickname, cellphoneNum, loginPw);
     }
 
     public void memberDelStatus(int loginedMemberId) {
@@ -49,5 +59,49 @@ public class MemberService {
 
     public void memberReStatus(int loginedMemberId) {
         memberRepository.memberReStatus(loginedMemberId);
+    }
+
+    // 인증코드 생성
+    public String generateAuthCode() {
+        return String.format("%06d", random.nextInt(999999));
+    }
+
+    // 이메일 전송
+    public void sendAuthCode(String email, String authCode) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("인증 코드");
+        message.setText("회원님의 인증 코드는 다음과 같습니다 : " + authCode);
+        mailSender.send(message);
+    }
+
+    public Member getMemberByEmail(String email) {
+        return memberRepository.getMemberByEmail(email);
+    }
+
+    public void sendLoginId(String email, String loginId) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("TailsRoute");
+        message.setText("회원님의 아이디는 다음과 같습니다 : " + loginId);
+        mailSender.send(message);
+    }
+
+    public void sendLoginPW(String email, String loginPW) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(email);
+        message.setSubject("TailsRoute");
+        message.setText("임시 비밀번호가 발급되었습니다 : " + loginPW);
+        mailSender.send(message);
+    }
+
+    // 랜덤 비밀번호 생성 메서드
+    public String generateRandomPassword() {
+        // UUID를 사용해 랜덤 문자열 생성 (하이픈 제거)
+        return UUID.randomUUID().toString().replace("-", "").substring(0, 12); // 12자리 문자열 반환
+    }
+
+    public void setTemporaryPassword(int id, String loginPW) {
+        memberRepository.setTemporaryPassword(id, loginPW);
     }
 }
